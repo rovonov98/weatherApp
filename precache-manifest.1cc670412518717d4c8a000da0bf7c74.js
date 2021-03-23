c2531f6042ec57aa5cc9600acd9988e7f3d1ca98
rovonov98 <stariyakk121@gmail.com>
2021-03-23 14:33:23 +0500

    added pwa

---
+++ precache-manifest.1cc670412518717d4c8a000da0bf7c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0dc8a0c3ec75c54f3c7",
+    "url": "/weatherApp/css/app.c53f4c96.css"
+  },
+  {
+    "revision": "dac17a20c0aae811b3149bc3226844f1",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "c0dc8a0c3ec75c54f3c7",
+    "url": "/weatherApp/js/app.42ababb5.js"
+  },
+  {
+    "revision": "dae62b2cd0e4dae2a030",
+    "url": "/weatherApp/js/chunk-vendors.74a7957d.js"
+  },
+  {
+    "revision": "3e93fd572f152b65c54d8e0aa7d788c6",
+    "url": "/weatherApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/weatherApp/robots.txt"
+  }
+]);
