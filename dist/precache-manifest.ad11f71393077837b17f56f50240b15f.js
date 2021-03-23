d57cc10933e7c49a8f0bb83ce2d669ef020dccf8
rovonov98 <stariyakk121@gmail.com>
2021-03-23 14:30:13 +0500

    added pwa

---
+++ dist/precache-manifest.ad11f71393077837b17f56f50240b15f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d155d103942d974135",
+    "url": "/css/app.c53f4c96.css"
+  },
+  {
+    "revision": "54bacd5f0f0e94bd1be3b024d6c87d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d155d103942d974135",
+    "url": "/js/app.93f194b7.js"
+  },
+  {
+    "revision": "dae62b2cd0e4dae2a030",
+    "url": "/js/chunk-vendors.74a7957d.js"
+  },
+  {
+    "revision": "3e93fd572f152b65c54d8e0aa7d788c6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
